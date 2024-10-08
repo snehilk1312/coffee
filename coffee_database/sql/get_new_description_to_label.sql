ba2e398c9a7b773a711a4883ba525451a1194d83
snehilk1312 <snehilkumar14072000@gmail.com>
2024-10-08 06:10:13 +0530

    do ner, improve ner script

--- coffee_database/sql/get_new_description_to_label.sql
+++ coffee_database/sql/get_new_description_to_label.sql
@@ -11,7 +11,7 @@ where roaster = 'savorworks'
   and name in (select name
                from coffee_first_scraped_at_1
                where rn = 1
-                 and scraped_at >= current_date-20)),
+                 and scraped_at >= current_date)),
 coffee_first_scraped_at_2 as (select *, row_number() over (partition by "Name" order by scraped_at) as rn
                                  from raw_scraped.quick_brown_fox
                                  ),
@@ -22,7 +22,7 @@ where roaster = 'quick_brown_fox'
   and name in (select "Name"
                from coffee_first_scraped_at_2
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_3 as (select *, row_number() over (partition by "name" order by scraped_at) as rn
@@ -35,7 +35,7 @@ where roaster = 'blue_tokai'
   and name in (select "name"
                from coffee_first_scraped_at_3
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_4 as (select *, row_number() over (partition by "Name" order by scraped_at) as rn
@@ -48,7 +48,7 @@ where roaster = 'corridor_seven'
   and name in (select "Name"
                from coffee_first_scraped_at_4
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_5 as (select *, row_number() over (partition by "Name" order by scraped_at) as rn
@@ -61,7 +61,7 @@ where roaster = 'curious_life'
   and name in (select "Name"
                from coffee_first_scraped_at_5
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_6 as (select *, row_number() over (partition by "Product Name" order by scraped_at) as rn
@@ -74,7 +74,7 @@ where roaster = 'kapi_kottai'
   and name in (select "Product Name"
                from coffee_first_scraped_at_6
                where rn = 1
-               and scraped_at >=  current_date-5
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_7 as (select *, row_number() over (partition by "Name" order by scraped_at) as rn
@@ -100,7 +100,7 @@ where roaster = 'koffie_genetics'
   and name in (select "name"
                from coffee_first_scraped_at_8
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_9 as (select *, row_number() over (partition by "Name of Coffee" order by scraped_at) as rn
@@ -113,7 +113,7 @@ where roaster = 'naivo'
   and name in (select "Name of Coffee"
                from coffee_first_scraped_at_9
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_10 as (select *, row_number() over (partition by "Name" order by scraped_at) as rn
@@ -126,7 +126,7 @@ where roaster = 'quick_brown_fox'
   and name in (select "Name"
                from coffee_first_scraped_at_10
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_11 as (select *, row_number() over (partition by "Name" order by scraped_at) as rn
@@ -139,7 +139,7 @@ where roaster = 'greysoul'
   and name in (select "Name"
                from coffee_first_scraped_at_11
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
                )
 ),
 coffee_first_scraped_at_12 as (select *, row_number() over (partition by "name" order by scraped_at) as rn
@@ -152,7 +152,20 @@ where roaster = 'rossette'
   and name in (select "name"
                from coffee_first_scraped_at_12
                where rn = 1
-               and scraped_at >=  current_date-20
+               and scraped_at >=  current_date
+               )
+),
+coffee_first_scraped_at_13 as (select *, row_number() over (partition by "name" order by scraped_at) as rn
+                                 from raw_scraped.fraction9
+                                 ),
+roaster13 as (
+select roaster,name,description
+from public.transformed_stg
+where roaster = 'fraction9'
+  and name in (select "name"
+               from coffee_first_scraped_at_13
+               where rn = 1
+               and scraped_at >=  current_date
                )
 ),
 final as (
@@ -167,8 +180,8 @@ union all
 select * from roaster5
 union all
 select * from roaster6
--- union all
--- select * from roaster7
+union all
+select * from roaster7
 union all
 select * from roaster8
 union all
@@ -179,5 +192,7 @@ union all
 select * from roaster11
 union all
 select * from roaster12
+union all
+select * from roaster13
 )
 select description from final
